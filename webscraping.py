d50eb960c3a4f37e3af4376adb418815c0bea476
Kalpit Jain <41207323+kalpitjain0407@users.noreply.github.com>
2018-11-11 14:46:04 +0530

    Add files via upload

---
+++ webscraping.py
@@ -0,0 +1,15 @@
+import re
+import urllib.request
+#https://en.oxforddictionaries.com/definition/car
+word = input("Enter a word : ")
+url = "https://en.oxforddictionaries.com/definition/"
+url = url + word
+print(url)
+data = urllib.request.urlopen(url).read()
+data1 = data.decode("utf-8")
+print(data1)
+m = re.search('<meta name="description" content="',data1)
+start = m.end()
+end = start + 150
+newString = data1[start:end]
+print(newString)
